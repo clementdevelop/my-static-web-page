be35a38bb31721cf8f663adb9912231bf00f9b9b
clementdevelop <clementdevelop@gmail.com>
2025-03-28 23:26:32 +0000

    Change base path - Update vue.config.js
    Change base path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-static-web-page/'  // GitHub Pages base path
+    ? '/'  // GitHub Pages base path
     : '/',
   devServer: {
     allowedHosts: "all",
